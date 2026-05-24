eca4ec8793aee89d7bf70024d4716fe8cf764a89
isacici328-ux <isacici328@gmail.com>
2026-05-24 15:46:22 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+{
+  "name": "vogaria-bot",
+  "version": "1.0.0",
+  "description": "Bot Discord Tribunal de Vogaria",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.15.0",
+    "firebase-admin": "^12.0.0"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
